3d8c029118f757ee1a27becf5b1851964a70f44a
[ghayastanoli] <[ghiyastanoli@yahoo.com]>
2025-01-12 00:00:06 +0500

    chore: react page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -25,9 +25,10 @@ const JobPage = () => {
     }
 
     return (
-        <div className="max-w-4xl mx-auto p-2">
-            <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
-            <div className="bg-white rounded-lg shadow-lg p-6">
+        <>
+        <div className=" mx-5 max-2-3xl p-2 text-white flex gap-3">
+            <div className="bg-gradient-to-t from-[#536e5f] to-[#b9ccc1]  rounded-lg shadow-xl p-6">
+            <h1 className="text-3xl font-bold mb-4 text-[white]">{job.title}</h1>
                 <p className="text-lg mb-2">{job.type}</p>
                 <p className="text-gray-600 mb-4">{job.location}</p>
                 <p className="text-xl font-semibold mb-4">{job.salary}</p>
@@ -36,7 +37,12 @@ const JobPage = () => {
                     <p>{job.description}</p>
                 </div>
             </div>
+            <div className='bg-gradient-to-t from-[#536e5f] to-[#b9ccc1]  rounded-lg shadow-xl p-6'>
+                <h3>About the Company</h3>
+                <h4>{job.company.name}</h4>
+            </div>
         </div>
+        </>
     )
 }
 
